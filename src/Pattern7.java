f73afa128df2d95f6397e309bbaa35705a7826b4
mustafakamran01 <kamran000mustafa@gmail.com>
2025-10-19 12:17:22 +0530

    Pattern question

---
+++ src/Pattern7.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Pattern7 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter number of rows: ");
+        int rows = scanner.nextInt();
+
+        char count = 'A';
+
+        for(int i = 1; i <= rows; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(count + " ");
+                count++;
+            }
+            System.out.println();
+        }
+    }
+}
