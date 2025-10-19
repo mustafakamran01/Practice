ea211957f671dab6672d3457053a27ecac03930b
mustafakamran01 <kamran000mustafa@gmail.com>
2025-10-19 11:30:54 +0530

    Pattern question

---
+++ src/Pattern5.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Pattern5 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+
+        if(rows <= 1){
+            System.out.println("Enter number of rows more than 1");
+        } else {
+            for(int i = 1; i <= rows; i++){
+                for(int j = 1; j <= (rows-i); j++){
+                    System.out.print(" ");
+                }
+                for(int k = 1; k <= i; k++){
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
