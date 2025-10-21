731281d9aa836f6ab1c3c91a79feb3e7b7071f32
mustafakamran01 <kamran000mustafa@gmail.com>
2025-10-21 15:21:41 +0530

    Pattern question

---
+++ src/Pattern10.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Pattern10 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter number of rows: ");
+        int rows = scanner.nextInt();
+
+        for(int i = 1; i <= rows; i++){
+            for(int j = 1; j <= rows; j++){
+                if (i == 1 || i == rows){
+                    System.out.print("* ");
+                } else {
+                    if (j == 1 || j == rows || j == i){
+                        System.out.print("* ");
+                    } else {
+                        System.out.print("  ");
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+/*
+    PATTERN:
+
+    * * * * * * * * * *
+    * *               *
+    *   *             *
+    *     *           *
+    *       *         *
+    *         *       *
+    *           *     *
+    *             *   *
+    *               * *
+    * * * * * * * * * *
+ */
