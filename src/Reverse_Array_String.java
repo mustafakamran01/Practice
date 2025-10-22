65e28503ebf8e8c80ff2b44085a3ef99524b43b8
mustafakamran01 <kamran000mustafa@gmail.com>
2025-10-22 11:26:17 +0530

    Reverse_Array_String

--- src/Reverse_Array_String.java
+++ src/Reverse_Array_String.java
@@ -5,18 +5,23 @@ public class Reverse_Array_String {
 
         Scanner scanner = new Scanner(System.in);
 
+        // Taking the number of string user wants to give
         System.out.println("Enter the number of strings you want to give: ");
         int length = scanner.nextInt();
 
+        // Creating new array to store user given string
         String[] str = new String[length];
 
+        // Storing string given by user
         for(int i = 0; i < length; i++){
             System.out.println("Enter " + (i+1) + " string");
             str[i] = scanner.next();
         }
 
+        // Creating new array to store reversed string
         String[] newStr = new String[length];
 
+        // Reversing string and storing into new array
         for (int i = 0; i < str.length; i++) {
             String s = str[i];
             StringBuilder stringBuilder = new StringBuilder();
@@ -26,6 +31,7 @@ public class Reverse_Array_String {
             newStr[i] = stringBuilder.toString();
         }
 
+        // Printing new array containing reversed string
         for(String ele: newStr){
             System.out.println("Element: " + ele + " ");
         }
