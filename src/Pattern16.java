0efd71abdb50d953c758cd686acf19e3d086bc6e
mustafakamran01 <kamran000mustafa@gmail.com>
2025-10-23 11:13:14 +0530

    Pattern Question

--- src/Pattern16.java
+++ src/Pattern16.java
@@ -8,13 +8,19 @@ public class Pattern16 {
         System.out.println("Enter number of rows: ");
         int rows = scanner.nextInt();
 
-        int part = rows/2;
+//        int part = rows/2;
 
-        for (int i = 1; i <= part+1; i++){
+        for (int i = 1; i <= rows; i++){
             for (int j = 1; j <= i; j++){
-                System.out.print("*");
+                System.out.print("* ");
             }
-
+            for (int j = i; j < rows; j++){
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
         }
 
 
