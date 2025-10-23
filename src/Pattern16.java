d972377cd7b13186623415b0c04dcd86c264f3c5
mustafakamran01 <kamran000mustafa@gmail.com>
2025-10-23 12:03:27 +0530

    Pattern Question

--- src/Pattern16.java
+++ src/Pattern16.java
@@ -8,89 +8,42 @@ public class Pattern16 {
         System.out.println("Enter number of rows: ");
         int rows = scanner.nextInt();
 
-//        int part = rows/2;
-
         for (int i = 1; i <= rows; i++){
-            for (int j = 1; j <= i; j++){
-                System.out.print("* ");
-            }
-            for (int j = i; j < rows; j++){
-                System.out.print("  ");
-            }
-            for (int j = 1; j <= i; j++){
-                System.out.print("* ");
+            for (int j = 1; j <= 2*rows; j++){
+                if (j > i && j <= 2*rows-i){
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
             }
             System.out.println();
         }
 
-
+        for (int i = rows; i >= 1; i--){
+            for (int j = 1; j <= 2*rows; j++){
+                if (j > i && j <= 2*rows-i){
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
     }
 }
-//*           *
-//*  *     *  *
-//*  *  *  *  *
-//*  *     *  *
-//*           *
+/*
+    PATTERN:
 
-//       *
-//    *  *
-// *  *  *
-//    *  *
-//       *
+    *        *
+    **      **
+    ***    ***
+    ****  ****
+    **********
+    **********
+    ****  ****
+    ***    ***
+    **      **
+    *        *
+ */
 
-//import java.util.Scanner;
-//
-//public class Pattern16 {
-//    public static void main(String[] args) {
-//
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Enter number of rows: ");
-//        int rows = scanner.nextInt();
-//
-//        // Upper half
-//        for (int i = 1; i <= rows; i++) {
-//            // Left stars
-//            for (int j = 1; j <= i; j++) {
-//                System.out.print("* ");
-//            }
-//
-//            // Spaces in the middle
-//            for (int j = i; j < rows; j++) {
-//                System.out.print("  ");
-//            }
-//            for (int j = i; j < rows; j++) {
-//                System.out.print("  ");
-//            }
-//
-//            // Right stars
-//            for (int j = 1; j <= i; j++) {
-//                System.out.print("* ");
-//            }
-//
-//            System.out.println();
-//        }
-//
-//        // Lower half
-//        for (int i = rows; i >= 1; i--) {
-//            // Left stars
-//            for (int j = 1; j <= i; j++) {
-//                System.out.print("* ");
-//            }
-//
-//            // Spaces in the middle
-//            for (int j = i; j < rows; j++) {
-//                System.out.print("  ");
-//            }
-//            for (int j = i; j < rows; j++) {
-//                System.out.print("  ");
-//            }
-//
-//            // Right stars
-//            for (int j = 1; j <= i; j++) {
-//                System.out.print("* ");
-//            }
-//
-//            System.out.println();
-//        }
-//    }
-//}
+
